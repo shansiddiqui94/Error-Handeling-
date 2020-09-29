db9f8ec945917d9ff4ebeae58e92be145bff9579
shansiddiqui94 <49912126+shansiddiqui94@users.noreply.github.com>
2020-09-28 23:21:32 -0400

    Add files via upload

---
+++ Error/script.js
@@ -0,0 +1,108 @@
+// Shan Siddiqui Error Handeling assigment.
+
+//Run this code first and examine the error message.
+//Fix the syntax error then run the code again to check your work.
+//#1
+///let launchReady = false;
+// let fuelLevel = 17000;
+
+// if (fuelLevel >= 20000) {
+//    console.log('Fuel level cleared.');
+//    launchReady = true;
+// } else {
+//    console.log('WARNING: Insufficient fuel!');
+//    launchReady = false;
+// }
+
+//#2
+
+// let launchReady = false;
+// let crewStatus = true;
+// let computerStatus = 'green';
+
+// if (crewStatus && computerStatus === 'green') {
+//    console.log('Crew & computer cleared.');
+//    launchReady = true;
+// }
+//  else{
+//    console.log('WARNING: Crew or computer not ready!');
+//    launchReady = false;
+// }
+
+// if (launchReady) {
+//    console.log("10, 9, 8, 7, 6, 5, 4, 3, 2, 1...");
+//    console.log("Fed parrot...");
+//    console.log("Ignition...");
+//    console.log("Liftoff!");
+//    }
+//    else {
+//    console.log("Launch scrubbed.");
+// }
+
+//#3
+
+/// let launchReady = false;
+// let fuelLevel = 17000;
+
+// if (fuelLevel >= 20000) {
+//    console.log('Fuel level cleared.');
+//    launchReady = true;
+//    } 
+// else{
+//    console.log('WARNING: Insufficient fuel!');
+//    launchReady = false;
+// }
+
+//#4
+
+/// let launchReady = false;
+// let fuelLevel = 27000;
+
+// if (fuelLevel >= 20000) {
+//    console.log('Fuel level cleared.');
+//    launchReady = true;
+// } else {
+//    console.log('WARNING: Insufficient fuel!');
+//    launchReady = false;
+// }
+
+// if (launchReady) {
+//    console.log("10, 9, 8...");
+//    console.log("Fed parrot...");
+//    console.log("6, 5, 4...");
+//    console.log("Ignition...");
+//    console.log("3, 2, 1...");
+//    console.log("Liftoff!");
+// } else {
+//    console.log("Launch scrubbed.");
+// }
+
+//#5
+
+// let launchReady = false;
+// let fuelLevel = 17000;
+// let crewStatus = true;
+// let computerStatus = 'green';
+
+// if (fuelLevel >= 20000) {
+//    console.log('Fuel level cleared.');
+//    launchReady = true;
+// } else {
+//    console.log('WARNING: Insufficient fuel!');
+//    launchReady = false;
+// }
+
+// if (crewStatus && computerStatus === 'green'){
+//    console.log('Crew & computer cleared.');
+//    launchReady = true;
+// } else {
+//    console.log('WARNING: Crew or computer not ready!');
+//   console.log(launchReady = false);
+// }
+
+// if (launchReady) {
+//    console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
+//    console.log('Liftoff!');
+// } else {
+//    console.log('Launch scrubbed.');
+// }
